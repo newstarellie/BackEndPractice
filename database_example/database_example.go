d8196403a791e652c369f8dc0762177ccf2aa1fa
elliechen <elliechen@lulainno.com>
2024-02-04 18:23:59 +0800

    change : database_example.go 成功創建表格 成功插入資料

--- database_example/database_example.go
+++ database_example/database_example.go
@@ -1,3 +1,4 @@
+// cd database_example
 // go run database_example.go
 
 package main
@@ -23,7 +24,39 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	// 連接成功，輸出成功訊息
 	fmt.Println("成功連接到資料庫")
+
+	// 創建表格
+	createTableStmt := `
+		CREATE TABLE IF NOT EXISTS example_table (
+			column1 TEXT,
+			column2 TEXT,
+			column3 TEXT
+		);
+	`
+
+	_, err = db.Exec(createTableStmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("成功創建表格")
+
+	// 插入資料
+	insertStmt := `
+		INSERT INTO example_table (column1, column2, column3)
+		VALUES (?, ?, ?)
+	`
+
+	// 替換下面這一行中的 value1, value2, value3
+	value1 := "your_value1"
+	value2 := "your_value2"
+	value3 := "your_value3"
+
+	_, err = db.Exec(insertStmt, value1, value2, value3)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("成功插入資料")
 }
